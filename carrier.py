55e038863675ce77245d4a294dd03b081976a5ac
CyberArchitect777 <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-22 10:52:49 +0100

    feat: create new carrier class

---
+++ carrier.py
@@ -0,0 +1,10 @@
+
+class Carrier():
+
+    def __init__(self, owner):
+        self.owner = owner
+        self.xlocation = -1
+        self.ylocation = -1
+        self.damage = 0
+        self.fuel = 100
+        self.supplies = 100
