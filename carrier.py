d598e3a68068b011dcf134720fe657f9cdc0632a
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-24 14:05:01 +0100

    chore: fully document the carrier.py class

--- carrier.py
+++ carrier.py
@@ -1,7 +1,29 @@
 
 class Carrier():
 
+    """
+        Carrier is a class that contains the attributes and methods required to represent a carrier in the game.
+
+        Attributes:
+            owner (int): An integer representing the owner of the carrier
+            xlocation (int): The X coordinate location of the carrier
+            ylocation (int): The Y coordinate location of the carrier
+            damage (int): The amount of damage the carrier has taken
+            fuel (int): The fuel in percentage the carrier has left
+            supplies (int): The current supply percentage of the carrier
+
+        Method:
+
+    """
+
     def __init__(self, owner):
+        """
+            Initalises the starting values of each carrier
+
+            Arguments: 
+                owner (int): Contains the owner of the carrier upon creation
+
+        """
         self.owner = owner
         self.xlocation = -1
         self.ylocation = -1
