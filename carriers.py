a8451ea6bbabda0774c4de77d01671a26e184e27
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-24 14:47:10 +0100

    feat: create a new carriers class to represent all carriers in the game

---
+++ carriers.py
@@ -0,0 +1,14 @@
+
+from carrier import *
+
+class Carriers():
+    """
+    Creates and manages a collection of all carriers in the game
+    """
+    def __init__(self):
+        """
+        Creates a fresh Carriers object and prepares a list to hold the collection of Carrier objects
+        """
+        self.carriers_list = [] # The carriers storage list
+
+    
