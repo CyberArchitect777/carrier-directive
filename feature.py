d1e1949d7fa4cc0d3593804bca7c057e7fdaa0f0
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-09-20 12:13:28 +0100

    chore: add additional comments on feature types to the feature.py file

--- feature.py
+++ feature.py
@@ -11,3 +11,12 @@ class Feature():
         self.x_location = x_location
         self.y_location = y_location
         self.feature_number = feature_number
+        # Feature types specified by feature_number above
+        # 2 - Command Center
+        # 3 - Lasers Turret (powerful against aircraft and hovercrafts)
+        # 4 - Anti-Aircraft guns (powerful against aircraft)
+        # 5 - Rocket Launchers (powerful against hovercrafts)
+        # 6 - Drone base
+        # 7 - Radar systems
+        # 8 - Fuel depot
+        # 9 - Materials warehouse
